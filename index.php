665ed64c585c19ac5ee53c8e6599908d0e17d3ba
bartek-flp <filipiuk.bartek@gmail.com>
2016-03-17 12:25:31 +0100

    frontend files

---
+++ index.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Meal Creator 0.1</title>
+
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<div class="container">
+  <div class="page-header">
+    <h1>Welcome to Meal Creator ver. 1.0-alpha1</h1>
+  </div>
+  <table class="table table-hover">
+    <caption>Products</caption>
+    <thead>
+    <tr>
+      <th>Name</th>
+      <th>Protein</th>
+      <th>Carbs</th>
+      <th>Fat</th>
+      <th>kCal</th>
+      <th>Unit</th>
+    </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>EMPTY</td>
+        <td>EMPTY</td>
+        <td>EMPTY</td>
+        <td>EMPTY</td>
+        <td>EMPTY</td>
+        <td>EMPTY</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <div class="center-block border">
+    <div>
+
+    </div>
+  </div>
+</div>
+</body>
+</html>
